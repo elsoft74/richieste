183d34a6d99fd2f52b34e6a76a848ab73bb582c3
Ivo Pugliese <elsoft74@gmail.com>
2022-05-18 01:04:29 +0200

    Update insertRequest.php
    Commentato codice di debug php

--- be/insertRequest.php
+++ be/insertRequest.php
@@ -1,7 +1,7 @@
 <?php
-    ini_set('display_startup_errors', 1);
-    ini_set('display_errors', 1);
-    error_reporting(-1);
+    //ini_set('display_startup_errors', 1);
+    //ini_set('display_errors', 1);
+    //error_reporting(-1);
     $out = new stdClass();
     $out->status = "KO";
     include_once("config/config.php");
