c1b630b7b7369aa469cb2fbab1f3b196030c0c2e
Ivo Pugliese <elsoft74@gmail.com>
2022-06-09 15:59:35 +0200

    Ridotte righe nella tabella

--- js/requests.js
+++ js/requests.js
@@ -9,7 +9,7 @@ function showRequests(richieste, user) {
         addRowPos: "top",          //when adding a new row, add it to the top of the table
         history: true,             //allow undo and redo actions on the table
         pagination: "local",       //paginate the data
-        paginationSize: 15,         //allow 7 rows per page of data
+        paginationSize: 12,         //allow 7 rows per page of data
         paginationCounter: "rows", //display count of paginated rows in footer
         movableColumns: true,      //allow column order to be changed
         initialSort: [             //set the initial sort order of the data
