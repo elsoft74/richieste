70485497348aabe88ab68dfbe605ec5de23680dd
Ivo Pugliese <elsoft74@gmail.com>
2022-06-11 09:16:43 +0200

    Aggiunta API per la data dell'ultimo aggiornamento di una tabella

--- be/classes/db.php
+++ be/classes/db.php
@@ -15,5 +15,32 @@
             //file_put_contents("../log/dbtest.log",(new DateTime("now"))->format("Y-m-d H:i").$msg."\n",FILE_APPEND);
             return $conn;
         }
+
+        public static function get_last_update_time($table) {
+            $out = new stdClass();
+            $out->status="KO";
+            $conn = DB::conn();
+            if ($conn != null){
+                try {
+                    $query = "SELECT count(*) AS exist FROM `information_schema`.tables WHERE TABLE_SCHEMA = 'richieste' AND TABLE_NAME = '$table' ";
+                    
+                    $stmt = $conn->prepare($query);
+                    $stmt->execute();
+                    $res=$stmt->fetch(PDO::FETCH_ASSOC);
+                    if(!$res['exist']){
+                        throw new Exception("TABLE-NOT-EXIST");
+                    }
+                    $query = "SELECT UPDATE_TIME FROM `information_schema`.tables WHERE TABLE_SCHEMA = '".DBNAME."' AND TABLE_NAME = '$table' ";
+                    $stmt = $conn->prepare($query);
+                    $stmt->execute();
+                    $res=$stmt->fetch(PDO::FETCH_ASSOC);
+                    $out->data=$res["UPDATE_TIME"];
+                    $out->status="OK";
+                } catch(Exception $ex){
+                        $out->error=$ex->getMessage();
+                    }
+                return $out;
+            }
+        }
     }
 ?>
